96df93861a3eec25afec9d134411fe770ff411b6
afleones <afleones@icloud.com>
2026-03-04 19:24:17 -0500

    Bug Fix: arreglando rutas en api/send-email.ts. 2026-03-04 - 19:00

--- api/send-email.ts
+++ api/send-email.ts
@@ -1,8 +1,8 @@
 import type { VercelRequest, VercelResponse } from '@vercel/node';
 import { Resend } from 'resend';
 import { render } from '@react-email/render';
-import ClientEmail from '../src/app/components/emails/ClientEmail.js';
-import AdminEmail from '../src/app/components/emails/AdminEmail.js';
+import ClientEmail from '../emails/ClientEmail'
+import AdminEmail from '../emails/AdminEmail'
 
 // Inicializar Resend
 const resend = new Resend(process.env.RESEND_API_KEY);
